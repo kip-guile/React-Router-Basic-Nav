296cfcc07f50f784a1572c45bde8c5aecc80b8f5
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-09-16 13:11:21 +0100

    paths displaying the proper components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
   return (
     <div>
-      <div className="App">
+      {/* <div className="App">
         <h1>React Router Mini</h1>
         <div>
           <a href="">Home</a>
@@ -14,7 +15,11 @@ const Navigation = () => {
         <div>
           <a href="">Contact</a>
         </div>
-      </div>
+      </div> */}
+
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
     </div>
   );
 };
